e76f3e53035e264ee80e6c98048b5cbb4d7e36d1
root <root@Maxim.localdomain>
2026-03-23 15:56:53 +0300

    Step 2: Connected the template engine

--- public/index.php
+++ public/index.php
@@ -2,16 +2,19 @@
 
 use Slim\Factory\AppFactory;
 use Slim\Psr7\Factory\ResponseFactory;
+use Slim\Views\PhpRenderer;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create(new ResponseFactory());
 
+$renderer = new PhpRenderer(__DIR__ . '/../templates');
+$renderer->setLayout('layout/app.phtml');
+
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function ($request, $response) {
-    $response->getBody()->write("Welcome to Slim Framework!");
-    return $response;
+$app->get('/', function ($request, $response) use ($renderer) {
+    return $renderer->render($response, 'index.phtml');
 });
 
 $app->run();
